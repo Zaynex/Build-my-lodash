b0708e0cbfaf9bb41968e8099e9072c6497026e9
zaynex <zaynex@126.com>
2017-10-21 23:55:07 +0800

    CHG: change plan

--- lodash/_chunk.js
+++ lodash/_chunk.js
@@ -5,12 +5,12 @@ import toInteger from './toInteger.js'
  * 以通过传入的参数数值为单位分割数字
  */
 
- /**
-  * 自己实现的
-  后来自己思考了下， 因为纯函数，如果修改了arr，arr在其他地方就已经变了
-  可以使用 JSON.parse(JSON.stringify()) 
-  但是对于数组比较大的时候显然比较消耗性能
-  */
+/**
+ * 自己实现的
+ 后来自己思考了下， 因为纯函数，如果修改了arr，arr在其他地方就已经变了
+ 可以使用 JSON.parse(JSON.stringify())
+ 但是对于数组比较大的时候显然比较消耗性能
+ */
 //  const chunk = (arr, size) => {
 //      let newArr = []
 //      while(arr.length) {
@@ -27,18 +27,18 @@ import toInteger from './toInteger.js'
 const nativeMax = Math.max  // 用于比较两个数的大小
 const nativeCeil = Math.ceil // 进位取整
 
-function chunk(array, size) {
+function chunk (array, size) {
     //  toInteger 转换成整数
     size = nativeMax(toInteger(size), 0)
-    const length = array == null ? 0: array.length
-    if(!length || size < 1) {
+    const length = array == null ? 0 : array.length
+    if (!length || size < 1) {
         return []
     }
     let index = -1
     let resIndex = 0
     // 思路比较清晰了。这里就是提前创建好数组的坑位 ceil 运算有余数时都会进一位
     const result = new Array(nativeCeil(length / size))
-    while(++index < length) {
+    while (++index < length) {
         //注意这里 传入的  index += size ，下一次返回的是  index+size
         result[resIndex++] = baseSlice(array, index, (index += size))
         // result[resIndex++] = array.slice(index, index+=size)
